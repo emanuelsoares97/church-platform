10d3938d44e649353fb4dfe50619cfcb60665985
emanuelsoares97 <tutasoares17@gmail.com>
2026-03-27 22:09:15 +0000

    movidas gallery management views para pacotes

--- management/views/__init__.py
+++ management/views/__init__.py
@@ -1,2 +1,11 @@
 from .legacy import *  # noqa: F401,F403
 from .dashboard import dashboard, events_hub, gallery_hub
+from cloudinary.uploader import destroy
+from .gallery import (
+	gallery_albums_list,
+	management_album_detail,
+	edit_gallery_album,
+	deactivate_gallery_album,
+	activate_gallery_album,
+	delete_gallery_album,
+)
